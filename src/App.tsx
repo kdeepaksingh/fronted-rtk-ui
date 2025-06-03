ad4e36024566e9775bf16714fb23d45527fe4498
Deepak Singh <princesingh8255@gmail.com>
2025-06-03 22:50:11 +0530

    this is the initial project setup commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { Button } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [age, setAge] = useState("");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChange = (event: any) => {
+    setAge(event.target.value);
+  };
   return (
     <>
       <div className="p-6 bg-gray-100 min-h-screen">
@@ -12,6 +25,23 @@ function App() {
           MUI Button
         </Button>
       </div>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="demo-select-small-label">Age</InputLabel>
+        <Select
+          labelId="demo-select-small-label"
+          id="demo-select-small"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
     </>
   );
 }
