fbdb6a688c9c9d0ad7bc9ba4a89a7d3b59c2ad22
Deepak Singh <princesingh8255@gmail.com>
2025-06-05 19:48:14 +0530

    changes in stringutils file

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,9 +1,9 @@
 export class StringUtils {
-static getImageName = (url: string): string => {
-    var index: number = url.lastIndexOf("/") + 1;
-    var imageName: string = url.substr(index);
+  static getImageName = (url: string): string => {
+    const index: number = url.lastIndexOf("/") + 1;
+    const imageName: string = url.substr(index);
     return imageName;
-};
+  };
 }
 
 export default StringUtils;
