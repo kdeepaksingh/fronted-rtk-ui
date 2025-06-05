970650e965d3f40f481f820d40d659a7d9052547
Deepak Kumar <deepak.kumar97@cipl.org.in>
2025-06-05 12:57:02 +0530

    worked on theme for all components

--- src/color.ts
+++ src/color.ts
@@ -1,7 +1,38 @@
 const colors = {
+  
+  /**
+   * Main colors
+   */
+
   "ui-black": "#202020",
-  "ui-white": "#ffffff",
+  "ui-brown-light": "#fae7df",
+  "ui-brown-light1": "#f3b28624",
+  "ui-brown-light2": "#1976D20D",
+  "ui-brown": "#9C3B00",
+  "ui-brown-scroll": "#6a2808",
+  "ui-brown-dark": "#882D00",
   "ui-blue": "#1d4ed8",
+  "ui-black-01": "#202E3B",
+  "ui-white": "#ffffff",
+  "ui-white-1": "#ffffff57",
+  "ui-primary": "#093F7C",
+  "ui-secondary": "#CCEFF9",
+  "ui-error": "#FF3B3B",
+  "ui-warning": "#FFA227",
+  "ui-info": "#005996",
+  "ui-green-dark": "#1A6734",
+  "ui-green": "#065f46",
+  "ui-gray": "#606060",
+  "ui-modal-gray": "#C7C7C",
+  "ui-card-light": "#FFF6F0",
+  "ui-card-light1": "#F1F1F1",
+  "ui-card-dark": "#E8651F",
+  "ui-danger": "#FF3B3B",
+  "ui-orange": "#db5f21",
+  "ui-border-orange": "#f5cdbc",
+  "ui-green-light": "#d1fae5",
+  "ui-red-light": "#fee2e2",
+  "ui-sea-light": "#e0f2fe",
 };
 
 export default colors;
--- src/theme.ts
+++ src/theme.ts
@@ -9,6 +9,245 @@ const theme = createTheme({
     fontSize: 18,
     body1: { lineHeight: 1, color: colors["ui-black"] },
   },
+  palette: {
+    primary: {
+      main: colors["ui-primary"],
+    },
+    secondary: {
+      main: colors["ui-secondary"],
+    },
+    error: {
+      main: colors["ui-error"],
+    },
+    warning: {
+      main: colors["ui-warning"],
+    },
+    info: {
+      main: colors["ui-info"],
+    },
+    success: {
+      main: colors["ui-green-dark"],
+    },
+  },
+  components: {
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: colors["ui-green-dark"],
+        },
+      },
+    },
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          overflow: "visible",
+          borderBottom: `1px solid`,
+          borderColor: colors["ui-orange"],
+        },
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            fontWeight: "700",
+            borderRadius: 4,
+            textTransform: "uppercase",
+          },
+        },
+      },
+    },
+    MuiInputBase: {
+      defaultProps: {
+        autoComplete: "off",
+      },
+      styleOverrides: {
+        root: {
+          fontSize: 16,
+          "&.MuiInputBase-root": {
+            background: "#fff",
+            fieldset: {
+              borderColor:colors["ui-orange"],
+            },
+            "&:focus": {
+              fieldset: {
+                borderColor: colors["ui-brown"],
+              },
+            },
+          },
+        },
+      },
+    },
+    MuiInput: {
+      defaultProps: {
+        size: "small",
+        autoComplete: "off",
+      },
+      styleOverrides: {
+        root: {
+          fieldset: {
+            borderColor: colors["ui-brown-dark"],
+          },
+        },
+      },
+    },
+    MuiTextField: {
+      variants: [
+        {
+          props: { size: "small" },
+          style: {
+            "& .MuiInputBase-root": {
+              fontSize: "15px",
+              padding: "2px",
+            },
+            "& .MuiInputLabel-root": {
+              fontSize: "15px",
+            },
+            "& .MuiInputBase-input": {
+              padding: "6.5px 10px",
+            },
+          },
+        },
+      ],
+      defaultProps: {
+        SelectProps: {
+          MenuProps: {
+            disablePortal: true,
+          },
+        },
+      },
+    },
+    MuiSelect: {
+      defaultProps: {
+        size: "small",
+        autoComplete: "off",
+        MenuProps: {
+          disablePortal: true,
+        },
+      },
+      styleOverrides: {
+        root: {
+          fieldset: {
+            borderColor: colors['ui-card-light'],
+          },
+          "&:hover": {
+            fieldset: {
+              borderColor: "#093F7C !important",
+            },
+          },
+        },
+      },
+    },
+    MuiCheckbox: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiTooltip: {
+      defaultProps: {
+        // disablePortal: true,
+        // Disable portal for all Tooltips
+      },
+      styleOverrides: {
+        popper: {
+          "z-index": "2000 !important",
+        },
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        className: "font-semibold uppercase",
+        size: "small",
+      },
+      styleOverrides: {
+        root: {
+          minWidth: 40,
+          fontWeight: 600,
+          "&.MuiButton-outlined:not([disabled])": {
+            background: "white",
+            borderWidth: "1.5px",
+          },
+          "&.MuiButtonBase-root:not(.MuiButton-sizeSmall)": {
+            minHeight: 36,
+          },
+          "&.Mui-disabled": {
+            boxShadow: "none",
+            color: "rgba(0, 0, 0, 0.26) !important",
+            backgroundColor: "transparent !important",
+          },
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          "&.MuiPaper-outlined": {
+            borderColor: colors["ui-brown"],
+          },
+        },
+      },
+    },
+    MuiDialog: {
+      defaultProps: {
+        // disablePortal: true,
+      },
+      styleOverrides: {
+        root: {
+          "& .MuiPaper-root": {
+            borderRadius: 10,
+          },
+        },
+      },
+    },
+    MuiTable: {
+      styleOverrides: {
+        root: {
+          "& .MuiTableCell-root:not(:last-child)": {
+            borderColor: colors["ui-brown"],
+            borderRight: `1px solid ${colors["ui-brown"]}`,
+          },
+          "& .MuiTableHead-root": {
+            background: colors["ui-brown-light"],
+            th: {
+              fontWeight: "600",
+            },
+          },
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: colors["ui-brown-light"],
+        },
+      },
+    },
+    MuiAutocomplete: {
+      defaultProps: {
+        // No unsupported props here
+      },
+    },
+    MuiPopover: {
+      defaultProps: {
+        // No unsupported props here
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        // No unsupported props here
+      },
+    },
+    MuiMenu: {
+      defaultProps: {
+        // No unsupported props here
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+  },
 });
 
 export default theme;
